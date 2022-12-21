0c56b703b906cf46668ab746374e427d58240321
Kieran Robinson <robinson.kieran@outlook.com>
2022-12-21 21:19:32 +0000

    Create AddActivityView and populate with UI elements

---
+++ Away-Day-Planner/Away-Day-Planner/Views/AddActivityView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Away_Day_Planner.Views
+{
+    public partial class AddActivityView : Form
+    {
+        public AddActivityView()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxRewardsRequired_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
