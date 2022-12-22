68a530999fdc475ef94d1aec3b54a08007344efd
Kieran Robinson <robinson.kieran@outlook.com>
2022-12-22 02:25:57 +0000

    Created interface IClientDepartmentSelectionView

--- Away-Day-Planner/Away-Day-Planner/Views/ClientDepartmentSelectionView.cs
+++ Away-Day-Planner/Away-Day-Planner/Views/ClientDepartmentSelectionView.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Away_Day_Planner.Views
 {
-    public partial class ClientDepartmentSelectionView : Form
+    public partial class ClientDepartmentSelectionView : Form, IClientDepartmentSelectionView
     {
         public ClientDepartmentSelectionView()
         {
