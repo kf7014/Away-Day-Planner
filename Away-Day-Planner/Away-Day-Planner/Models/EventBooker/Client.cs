dd7dca8f6314a939c59885afb7c494f2ec3d7889
Kieran Robinson <robinson.kieran@outlook.com>
2022-12-24 14:58:38 +0000

    Added and populated Client class

---
+++ Away-Day-Planner/Away-Day-Planner/Models/EventBooker/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Models.EventBooker
+{
+    class Client
+    {
+        private int Id;
+        public int id
+        {
+            get { return Id; }
+        }
+
+        //private ICollection<IDepartment> Departments;
+        //public ICollection<IDepartment> departments
+        //{
+        //    get { return Departments; }
+        //    set { Departments = value; }
+        //}
+
+
+        private bool HasClientDispute;
+        public bool hasClientDispute
+        {
+            get { return HasClientDispute; }
+            set { HasClientDispute = value; }
+        }
+
+        private int NoOfHoursAway;
+        public int noOfHoursAway
+        {
+            get { return NoOfHoursAway; }
+            set { NoOfHoursAway = value; }
+        }
+
+        private String Name;
+        public String name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+    }
+}
