6d7fed45f810aa3829b3e8d220032c603ccdcb00
Scrub <scott-donaldson@outlook.com>
2022-12-26 15:52:33 +0000

    Added the Database, databse conn string (change this) possibly making it dynamic so we dont have to. Added Basic entity context once we got classes that can be saved.

---
+++ Away-Day-Planner/Away-Day-Planner/Database/EntityContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Database
+{
+    internal class EntityContext : DbContext
+    {
+        public EntityContext() : base("name=connection-string")
+        {
+           // public DbSet<Ix> x { get; set;}
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Away-Day-Planner/Away-Day-Planner/Database/IDatabaseInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Database
+{
+    internal interface IDatabaseInterface
+    {
+
+    }
+}
