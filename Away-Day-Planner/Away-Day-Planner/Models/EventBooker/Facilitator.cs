3e06f76fe30b300ece475be2594c4be6a7a5ccab
Kieran Robinson <robinson.kieran@outlook.com>
2022-12-28 00:48:20 +0000

    Added and populated Facilitator class

---
+++ Away-Day-Planner/Away-Day-Planner/Models/EventBooker/Facilitator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Models.EventBooker
+{
+    public class Facilitator : IPerson
+    {
+        private int Id;
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        private String FirstName;
+        public String firstName
+        {
+            get { return FirstName; }
+            set { FirstName = value; }
+        }
+
+        private String LastName;
+        public String lastName
+        {
+            get { return LastName; }
+            set { LastName = value; }
+        }
+
+        private String EmailAddress;
+        public String emailAddress
+        {
+            get { return EmailAddress; }
+            set { EmailAddress = value; }
+        }
+    }
+}
