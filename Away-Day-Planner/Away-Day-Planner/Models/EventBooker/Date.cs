9c6ebedfb3f0056c27e7d6927bc713a4b02c54e8
Kieran Robinson <robinson.kieran@outlook.com>
2022-12-29 15:08:31 +0000

    Added Date class and Interface

---
+++ Away-Day-Planner/Away-Day-Planner/Models/EventBooker/Date.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Models.EventBooker
+{
+    class Date : IDate
+    {
+        private int Id;
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        private DateTime DateTime;
+        public DateTime dateTime
+        {
+            get { return DateTime; }
+            set { DateTime = value; }
+        }
+    }
+}
