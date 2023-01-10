ba968ee622266aa81a11ad078909987d973b5c1b
Kieran Robinson <robinson.kieran@outlook.com>
2023-01-10 16:21:01 +0000

    Added a DatabaseAbstraction class to utilise DatabaseInterface, and added addNewClient and addNewDepartment methods

--- Away-Day-Planner/Away-Day-Planner/Models/ClientDepartment/ClientModel.cs
+++ Away-Day-Planner/Away-Day-Planner/Models/ClientDepartment/ClientModel.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Away_Day_Planner.Database;
 
 namespace Away_Day_Planner.Models.ClientDepartment
 {
     public class ClientModel : IClientModel    
     {
+        DatabaseAbstraction databaseAbstraction = new DatabaseAbstraction();
         List<Client> clientList; 
         public ClientModel()
         {
+            //Create dummy database data at runtime
+            //TODO: This could be moved to program.cs, especially if theres a lot of classes doing it
+            databaseAbstraction.addNewClient("Kieran New Method", 1, false);
+            databaseAbstraction.addNewClient("Sandra New Method", 1, true);
+            
+            
+            
             clientList = new List<Client>();
-            clientList.Add(new Client(0, "Kieran", 1, false));
-            clientList.Add(new Client(1, "Sandra", 2, false));
 
-            //Add data to department dummy
-            clientList[0].departments.Add(new Department("Test Department"));
-            clientList[1].departments.Add(new Department("Sandras Department"));
+            //clientList.Add(new Client(0, "Kieran", 1, false));
+            //clientList.Add(new Client(1, "Sandra", 2, false));
+
+            ////Add data to department dummy
+            //clientList[0].departments.Add(new Department("Test Department"));
+            //clientList[1].departments.Add(new Department("Sandras Department"));
+
 
         }
 
@@ -36,7 +47,7 @@ namespace Away_Day_Planner.Models.ClientDepartment
 
         public Client[] getClientList()
         {
-            return clientList.ToArray();
+            return clientList.ToArray();                
         }
     }
 }
