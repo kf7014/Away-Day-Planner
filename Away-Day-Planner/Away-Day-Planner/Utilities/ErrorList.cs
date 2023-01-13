24d9eab5815c00b2ae44c4fb20f7056e5e750619
Scrub <scott-donaldson@outlook.com>
2023-01-13 17:26:47 +0000

    Added comments to the database interface to help with understanding its functionality

---
+++ Away-Day-Planner/Away-Day-Planner/Utilities/ErrorList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Utilities
+{
+    public class ErrorList
+    {
+        public static readonly Dictionary<string, Exception> Errors = new Dictionary<string, Exception>()
+        {
+
+        };
+        public static readonly Dictionary<string, Exception> DatabaseErrors = new Dictionary<string, Exception>()
+        {
+            {"InvalidType", new Exception("Oops, there was an unknown model type")},
+            {"TypeMismatch", new Exception("Oops, you cannot update an entity with a different object")},
+            {"NegativeID", new Exception("Oops, an ID cannot be negative")},
+            {"InvalidRange", new Exception("Oops, Upper range cannot be lower than lower range")},
+            {"NullEntity", new Exception("Object provided was null")},
+            {"NoID", new Exception("Object does not contain id field (Case sensitive)")},
+        };
+    }
+}
