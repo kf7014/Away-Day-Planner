526ba16c0ec9b64b343dd1ed7fdc7f246695cbe9
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2023-01-14 16:43:16 +0000

    Added new imple with factory to produce chain correctly

---
+++ Away-Day-Planner/Away-Day-Planner/EventChain/EventChainFactory.cs
@@ -0,0 +1,69 @@
+﻿using Away_Day_Planner.EventChain.Handlers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Forms;
+using Away_Day_Planner.Utilities;
+
+namespace Away_Day_Planner.EventChain
+{
+    internal class EventChainFactory
+    {
+        private Dictionary<string, Exception> Errors = ErrorList.Errors;
+        private BaseEventHandler NewBaseHandler()               => Activator.CreateInstance<BaseEventHandler>();
+        private PreliminaryEventHandler NewPreliminaryHandler() => Activator.CreateInstance<PreliminaryEventHandler>();
+        private GeneralEventHandler NewEventHandler()           => Activator.CreateInstance<GeneralEventHandler>();
+        private CancelledEventHandler NewCancelledHandler()     => Activator.CreateInstance<CancelledEventHandler>();
+        private PaymentEventHandler NewPaymentHandler()         => Activator.CreateInstance<PaymentEventHandler>();
+
+        private IEvent _Event = null;
+        private IHandler _Base = null;
+
+        public EventChainFactory(){}
+        public EventChainFactory(IEvent e){
+            _Event = e;
+            SetSuccessors();
+        }
+
+        public void SetEvent(IEvent e) { _Event = e; }
+        public IEvent GetEvent() { return _Event; }
+
+        public IHandler GetBase() { return _Base; }
+        public IHandler GetEntry() { return GetBase(); }
+        public IHandler GetStart() { return GetBase(); }
+
+
+        private void SetSuccessors() 
+        { 
+            /*
+             Layout and order of successors:
+
+             Base ---> Prelim ---> General ---> Payment
+               |          |           |           ^
+               |          |           |           |
+               -----------------------------> Cancelled            
+             */
+            if (_Event == null) throw Errors["Null"];
+            BaseEventHandler _base = NewBaseHandler();
+            PreliminaryEventHandler _prelim = NewPreliminaryHandler();
+            GeneralEventHandler _general = NewEventHandler();
+            CancelledEventHandler _cancel = NewCancelledHandler();
+            PaymentEventHandler _pay= NewPaymentHandler();
+
+            _base.SetCancelledSuccessor(_cancel);
+            _prelim.SetCancelledSuccessor(_cancel);
+            _general.SetCancelledSuccessor(_cancel);
+
+            _base.SetSuccessor(_prelim);
+            _prelim.SetSuccessor(_general);
+            _general.SetSuccessor(_pay);
+            _cancel.SetSuccessor(_pay);
+
+            _base.SetEvent(_Event);
+            _Base = _base;
+        }
+    }
+
+}
