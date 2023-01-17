be2b81b6a47007a87e040454e10e64d750abde4a
Sandra Czernik <w19009505@northumbria.ac.uk>
2023-01-17 14:20:53 +0000

    Added login folder to models. Added setters and getters relevant to login system.

---
+++ Away-Day-Planner/Away-Day-Planner/Models/Login/IUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Away_Day_Planner.Models.Login
+{
+    public interface IUser
+    {
+        int userID { get; set; }
+        String firstName { get; set; }
+        String lastName { get; set; }
+        String username { get; set; }
+        String userEmail { get; set; }
+        String userRole{ get; set; }
+
+}
+
+}
